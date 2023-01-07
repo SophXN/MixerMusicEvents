a1d669c4e98ef30be313c1b796d7de3cd5006b58
Sophie Nguyen <nguyen.x.sophia@gmail.com>
2023-01-07 14:16:29 -0500

    update setup and remove nested folders

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About HIHIHIH</h1>
+      <p>Musical Events Platform to find your local events!</p>
+      <p>Version 1.1.0</p>
+    </div>
+  );
+}
---
+++ pages/events/[slug].js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function EventPage() {
+  return (
+    <div>
+      <h1>Each Event Page</h1>
+    </div>
+  );
+}
